542bb5bceb101a05d037dc2815b76eeed347a82c
zenga2 <2459672025@qq.com>
2017-03-27 15:10:20 +0800

    commit code for date utils and string utils

--- utils-lib/src-es6/date.js
+++ utils-lib/src-es6/date.js
@@ -1,7 +1,32 @@
-function formate(date, fmt) {
-    if (/(y+)/.test(fmt)) {
+import {paddingLeft} from './string'
 
+// fmt eg: 'yyyy-MM-dd hh:mm:ss'
+function formate(date, fmt) {
+    let matchStr
+    let obj = {
+        '(M+)': date.getMonth() + 1,
+        '(d+)': date.getDay(),
+        '(h+)': date.getHours(),
+        '(m+)': date.getMinutes(),
+        '(s+)': date.getSeconds()
     }
+
+    if (/(y+)/.test(fmt)) {
+        matchStr = RegExp.$1
+        fmt = fmt.replace(matchStr, String(date.getFullYear()).slice(4 - matchStr.length))
+    }
+
+    for (let k in obj) {
+        if (new RegExp(k).test(fmt)) {
+            matchStr = RegExp.$1
+            let val = String(obj[k])
+            // 当匹配模式中的M|d|h|m|s为单个的话，则相应的值左边不补零
+            val = matchStr.length === 1 ? val : paddingLeft(val, '0', 2)
+            fmt = fmt.replace(matchStr, val)
+        }
+    }
+
+    return fmt
 }
 
 export {formate}
---
+++ utils-lib/src-es6/string.js
@@ -0,0 +1,18 @@
+function paddingLeft(str, padStr, totalLen) {
+    while (str.length < totalLen) {
+        str = padStr + str;
+    }
+
+    return str.slice(-totalLen)
+}
+
+function paddingRight(str, padStr, totalLen) {
+    while (str.length < totalLen) {
+        str += padStr;
+    }
+
+    return str.slice(0, totalLen)
+}
+
+export {paddingLeft, paddingRight}
+export default {paddingLeft, paddingRight}
