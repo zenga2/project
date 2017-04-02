efed8ba892d8d935811695612344ca6caa3d457e
zenga2 <2459672025@qq.com>
2017-04-03 04:19:47 +0800

    create express demo

---
+++ expressDemo/index.js
@@ -0,0 +1,59 @@
+var express = require('express')
+var appData = require('./data.json')
+
+var app = express()
+var router = express.Router()
+
+router.get('/', function (req, res, next) {
+    req.url = '/index.html'
+    next()
+})
+
+app.use(router)
+
+var seller = appData.seller
+var goods = appData.goods
+var ratings = appData.ratings
+var apiRoutes = express.Router()
+
+apiRoutes.get('/seller', function (req, res) {
+    console.log('seller')
+    res.json({
+        errno: 0,
+        data: seller
+    })
+})
+apiRoutes.post('/seller', function (req, res) {
+    console.log('seller')
+    res.json({
+        errno: 0,
+        data: seller
+    })
+})
+
+apiRoutes.get('/goods', function (req, res) {
+    console.log('goods')
+    res.json({
+        errno: 0,
+        data: goods
+    })
+})
+
+apiRoutes.get('/ratings', function (req, res) {
+    console.log('ratings')
+    res.json({
+        errno: 0,
+        data: ratings
+    })
+})
+
+app.use('/api', apiRoutes)
+app.use(express.static('./dist'))
+
+module.exports = app.listen('8080', function (err) {
+    if (err) {
+        console.log(err)
+        return
+    }
+    console.log('Listening at http://localhost:' + 8080 + '\n')
+})
